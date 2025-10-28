2e4654614f377a7266b4d3fbc20dcd553459b854
Julian Frumar <julian@frumar.com>
2025-10-28 15:22:34 -0700

    Add MUI themed sign-in form

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import styles from './home.module.css';
 
 export default function Home() {
   return (
+
     <div className={clsx(
       styles.home,
       'flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black')}>
