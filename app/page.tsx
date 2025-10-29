42fdb6a67e90dea1d46d5e120ba31239e06877ef
Julian Frumar <julian@frumar.com>
2025-10-28 21:46:14 -0700

    Cleanup unused files

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import { Container } from '@mui/material';
 import clsx from 'clsx';
 
-import styles from './home.module.css';
 import HomepageSignInLink from './ui/homepage-sign-in-link';
 
 export default function Home() {
@@ -9,7 +8,6 @@ export default function Home() {
 
   return (
     <div className={clsx(
-      styles.home,
       'flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black')}>
       <Container>
         <HomepageSignInLink />
